49f015caa4724c879ffa431a0beee19896846ba7
ricelectric <215439555+ricelectric@users.noreply.github.com>
2025-07-14 23:38:30 +0900

    Add files via upload

---
+++ arduino/blink/blink.ino
@@ -0,0 +1,13 @@
+#define LED_BUILTIN 15 //P1.5
+
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
